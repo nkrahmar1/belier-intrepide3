dab21d2c4008959a49da3afdcd94794cd8a7bc32
nkrahmar1 <krahmarieaimeenguessan39@gmail.com>
2025-11-12 19:12:00 +0100

    chore(tailwind): reactiver preflight pour faire de Tailwind la base de styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,14 @@ export default {
     "./resources/**/*.js",
     "./resources/**/*.vue",
   ],
+  /* Réactivation de Preflight : Tailwind sera la base principale de styles.
+     Si vous préférez que Bootstrap reste la base, remettre preflight: false.
+  */
+  // corePlugins: { preflight: false },
   theme: {
     extend: {},
   },
   plugins: [
     require('@tailwindcss/forms'),
   ],
-  plugins: [],
 }

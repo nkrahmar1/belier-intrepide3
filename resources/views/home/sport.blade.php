1584120d0bf6853b1c1ec25fc73726cb8a590640
nkrahmar1 <nkrahmar1@github.com>
2026-05-29 10:54:02 +0000

    fix:mise a jour de la dasboard

--- resources/views/home/sport.blade.php
+++ resources/views/home/sport.blade.php
@@ -5,10 +5,6 @@
 @section('content')
 
     <p1>Sport</p1>
-     <img src="{{ asset('image/pdci3.jpg') }}" alt="PDCI-RDA" style="
-        width: 100vw;
-        height: 400px;
-        object-fit: cover;
-        display: block;
+    
 
 @endsection
